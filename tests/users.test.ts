f01101579f6589f7ab4f04eb6075e71fc3f8afde
farias-77 <gabrielfalves2003@gmail.com>
2022-09-18 22:10:30 -0300

    tests sign-up route

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -16,5 +16,31 @@ describe("Testa SignUp", () => {
         expect(result.body).toBeInstanceOf(Object);
     });
 
+    it("Testa SignUp com body incompleto -> deve retornar 422", async () => {
+        
+        const user = userFactory.correctUserFactory();
+        user.email = "";
+        
+        const result = await supertest(app).post("/sign-up").send(user);
 
+        expect(result.status).toBe(422);
+    });
+
+    it("Testa SignUp com confirmPassword incorreta -> deve retornar 401", async () => {
+        const user = userFactory.correctUserFactory();
+        user.confirmPassword = user.confirmPassword + "a";
+        
+        const result = await supertest(app).post("/sign-up").send(user);
+
+        expect(result.status).toBe(401);
+    });
+
+    it("Testa SignUp com email já em uso ->, deve retornar 409", async () => {
+        const user = userFactory.correctUserFactory();
+        await supertest(app).post("/sign-up").send(user);
+
+        const result = await supertest(app).post("/sign-up").send(user);
+
+        expect(result.status).toBe(409);
+    });
 });
